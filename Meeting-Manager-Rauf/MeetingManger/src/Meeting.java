deda33a832368e28b91ee3e63402c45dc0fe3813
Nicole145 <37220316+Nicole145@users.noreply.github.com>
2018-03-26 14:17:34 +0100

    Add files via upload

---
+++ Meeting-Manager-Rauf/MeetingManger/src/Meeting.java
@@ -0,0 +1,183 @@
+import java.util.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author Nicole
+ *
+ */
+public class Meeting implements Comparable<Meeting>{
+	
+	private Date date;
+	private Time startTime;
+	private Time endTime;
+	private String description;
+	private ArrayList<Employee> attending;
+
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	/**
+	 * @return the startTime
+	 */
+	public Time getStartTime() {
+		return startTime;
+	}
+
+
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
+	}
+
+
+	/**
+	 * @return the endTime
+	 */
+	public Time getEndTime() {
+		return endTime;
+	}
+
+
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(Time endTime) {
+		this.endTime = endTime;
+	}
+
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	/**
+	 * @return the attending
+	 */
+	public ArrayList<Employee> getAttending() {
+		return attending;
+	}
+
+
+	/**
+	 * @param attending the attending to set
+	 */
+	public void setAttending(ArrayList<Employee> attending) {
+		this.attending = attending;
+	}
+
+
+	/**
+	 * Default constructor for Meeting
+	 */
+	public Meeting()
+	{
+		date = null;
+		startTime = null;
+		endTime = null;
+		description = "";
+		attending = new ArrayList<Employee>();
+	}
+	
+	/**
+	 * Alternative constructor for Meeting
+	 * @param date
+	 * @param startTime
+	 */
+	public Meeting(Date date, Time startTime)
+	{
+		this.date = date;
+		this.startTime = startTime;
+		endTime = null;
+		description = "";
+		attending = new ArrayList<Employee>();
+	}
+	
+	/**
+	 * Alternative constructor for Meeting
+	 * @param date
+	 * @param startTime
+	 * @param endTime
+	 * @param description
+	 * @param attending
+	 */
+	public Meeting(Date date, Time startTime, Time endTime, String description, ArrayList<Employee> attending)
+	{
+		this.date = date;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.description = description;
+		this.attending = attending;
+	}
+	
+	/**
+	 * Copy constructor for Meeting
+	 * @param meeting
+	 */
+	public Meeting(Meeting meeting)
+	{
+		date = meeting.getDate();
+		startTime = meeting.getStartTime();
+		endTime = meeting.getEndTime();
+		description = meeting.getDescription();
+		attending = new ArrayList<Employee>(meeting.getAttending());
+	}
+
+
+	@Override
+	public int compareTo(Meeting m2)
+	{
+		int compare = date.compareTo(m2.getDate());
+		if(compare == 0)
+		{
+			return startTime.compareTo(m2.getStartTime());
+		}
+		else
+		{
+			return compare;
+		}
+	}
+	
+	/**
+	 * Display meeting details on console
+	 */
+	public void display()
+	{
+		System.out.println(description);
+		System.out.println("Date: " + date);
+		System.out.println("Start Time: " + startTime);
+		System.out.println("End Time: " + endTime);
+	}
+}
---
+++ Meeting-Manager-Rauf/MeetingManger/src/Menu.java
@@ -0,0 +1,223 @@
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * 
+ */
+
+/**
+ * @author Nicole
+ *
+ */
+public class Menu {
+
+	private ArrayList<Employee> allEmployees;
+	private String fileName;
+	private Stack<String> editsMade;
+	private Meeting[] lastChange;
+	private Employee currentEmployee;
+	
+	/**
+	 * @return the allEmployees
+	 */
+	public ArrayList<Employee> getAllEmployees() {
+		return allEmployees;
+	}
+
+	/**
+	 * @param allEmployees the allEmployees to set
+	 */
+	public void setAllEmployees(ArrayList<Employee> allEmployees) {
+		this.allEmployees = allEmployees;
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	 * @return the editsMade
+	 */
+	public Stack<String> getEditsMade() {
+		return editsMade;
+	}
+
+	/**
+	 * @param editsMade the editsMade to set
+	 */
+	public void setEditsMade(Stack<String> editsMade) {
+		this.editsMade = editsMade;
+	}
+
+	
+	/**
+	 * @return the lastChange
+	 */
+	public Meeting[] getLastChange() {
+		return lastChange;
+	}
+
+	/**
+	 * @param lastChange the lastChange to set
+	 */
+	public void setLastChange(Meeting[] lastChange) {
+		this.lastChange = lastChange;
+	}
+
+	/**
+	 * @return the currentEmployee
+	 */
+	public Employee getCurrentEmployee() {
+		return currentEmployee;
+	}
+
+	/**
+	 * @param currentEmployee the currentEmployee to set
+	 */
+	public void setCurrentEmployee(Employee currentEmployee) {
+		this.currentEmployee = currentEmployee;
+	}
+
+	/**
+	 * Default constructor for Menu
+	 */
+	public Menu(String fileName)
+	{
+		this.fileName = fileName;
+		allEmployees = new ArrayList<Employee>();
+		editsMade = new Stack<String>();
+		lastChange = new Meeting[2];
+		currentEmployee = new Employee();
+	}
+	
+	
+	/**
+	 * Add meeting method for undo feature
+	 * @param toAdd
+	 */
+	public void addMeeting(Meeting toAdd)
+	{
+		for(Employee employee : toAdd.getAttending())
+		{
+			employee.getDiary().add(toAdd);
+		}
+		
+		lastChange[0] = toAdd;
+		editsMade.push("add");
+	}
+
+	/**
+	 * Delete meeting
+	 * @param toDelete
+	 */
+	public void deleteMeeting(Meeting toDelete)
+	{
+		for(Employee employee : toDelete.getAttending())
+		{
+			employee.getDiary().remove(toDelete);
+		}
+		
+		lastChange[0] = toDelete;
+		editsMade.push("delete");
+	}
+	
+	/**
+	 * Determines if changes can be made to meetings and edits them
+	 * @param toEdit Meeting to edit
+	 * @param date The changed date
+	 * @param startTime The changed start time
+	 * @param endTime The changed end time
+	 * @param description The changed description
+	 * @param attending The changed list of employees attending
+	 */
+	public void editMeeting(Meeting toEdit, Date date, Time startTime, 
+			Time endTime, String description, ArrayList<Employee> attending)
+	{
+		boolean valid = true;
+		
+		//checks if all employees are free for the new meeting date and time
+		boolean free = true;
+		for(Employee employee : attending)
+		{
+			free = employee.isFree(toEdit, date, startTime, endTime);
+			if(!free)
+			{
+				valid = false;
+				break;
+			}
+		}
+		
+		if(valid)
+		{
+			editsMade.push("edit");
+			Meeting beforeEdits = new Meeting(toEdit);
+			lastChange[0] = beforeEdits;
+			
+			if(!toEdit.getAttending().equals(attending) || !toEdit.getDate().equals(date) 
+					|| !toEdit.getStartTime().equals(startTime))
+			{
+				deleteMeeting(toEdit);
+				toEdit.setDate(date);
+				toEdit.setStartTime(startTime);
+				toEdit.setAttending(attending);
+				addMeeting(toEdit);
+			}
+			toEdit.setEndTime(endTime);
+			toEdit.setDescription(description);
+			
+			lastChange[1] = toEdit;
+		}
+		else
+		{
+			System.out.println("Cannot make these changes to the meeting as not all employees are available.");
+		}
+	}
+	
+	
+	/**
+	 * Undo feature
+	 */
+	public boolean undo()
+	{
+		boolean undo = false;
+		String editType = editsMade.pop();
+		//System.out.println(editType);
+		switch(editType)
+		{
+			case "add": 
+				deleteMeeting(lastChange[0]);
+				undo = true;
+				break;
+			
+			case "delete": 
+				addMeeting(lastChange[0]);
+				undo = true;
+				break;
+			
+			case "edit":
+				//delete meeting from after edits
+				for(Employee employee : lastChange[1].getAttending())
+				{
+					employee.getDiary().remove(lastChange[1]);
+				}
+				
+				addMeeting(lastChange[0]); //add meeting from before edits
+				undo = true;
+				break;
+		}
+		return undo;
+	}
+	
+}
